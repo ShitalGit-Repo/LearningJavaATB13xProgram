fa6b4a1610ed5132009bd256bfe5ff5302220e82
IML-WKS-Admin <awacharshital94@gmail.com>
2025-06-30 18:05:58 +0530

    Practised and pushes code on 30 June 2025

--- src/ex_01_Java_Basic_Variable/Lab007_print_println_printf.java
+++ src/ex_01_Java_Basic_Variable/Lab007_print_println_printf.java
@@ -1,4 +1,24 @@
 package ex_01_Java_Basic_Variable;
 
 public class Lab007_print_println_printf {
+    public static void main(String[] args){
+
+        int a = 10;
+
+        System.out.print("Print command without the new line");
+        System.out.println("Print command with new line");
+        System.out.printf("Your variable name is %d", a);
+
+        //%d = int, byte, long, short -- Data Types
+        //%s = string
+        //%f = float, double
+        //%b = boolean
+        //USE
+
+        System.out.println("___");
+
+        int b = 20;
+        System.out.printf("%d + %d",a,b);
+
+    }
 }
