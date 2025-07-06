e029a0363fbfc579edc532a76260cdffa1569451
IML-WKS-Admin <awacharshital94@gmail.com>
2025-07-06 17:16:05 +0530

    06 July 2025

---
+++ src/ex_10_Loops/Lab048_while_factorial.java
@@ -0,0 +1,28 @@
+package ex_10_Loops;
+
+import java.util.Scanner;
+
+public class Lab048_while_factorial {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Welcome to factorial program\nEnter the number");
+        //handle negative scenario like user entered the string or negative value.line no 10-13
+        if (!scanner.hasNextInt()){
+            System.out.println("Enter the integer");
+            return;                                        //handle for char if entered.
+        }
+        int number = scanner.nextInt();
+
+        int factorial = 1;
+        if(number<=0){
+            System.out.println("Negative factorial is not allowed");
+            return;                                                     //handle for negative value entered.
+        }else{
+            //calculate factorial
+            for(int i=1; i<=number; i++){
+                factorial = factorial*i;
+            }
+        }
+        System.out.println("Factorial is->"+factorial);
+    }
+}
---
+++ src/ex_10_Loops/Lab049_while_guessing_game.java
@@ -0,0 +1,36 @@
+package ex_10_Loops;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lab049_while_guessing_game {
+    public static void main(String[] args) {
+
+//Guessing game program - guess a no from 1-100
+        Random random = new Random();
+        int notoguess = random.nextInt(100)+1;
+//        System.out.println(notoguess);   //we have to keep it as comment else it will print the number.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+
+        int guess;
+        int attempts = 0;
+
+        while (true){
+            System.out.println("Enter another guess");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < notoguess){
+                System.out.println("Too low, try again");
+            } else if (guess > notoguess) {
+                System.out.println("Too high, try again");
+            }else {
+                System.out.println("Correct! you guess it in"+ attempts + "attempt");
+            }
+        }
+
+    }
+
+}
