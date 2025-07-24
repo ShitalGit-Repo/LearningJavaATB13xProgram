469b75323fc5ecb896a440f692dcb10251f47995
IML-WKS-Admin <awacharshital94@gmail.com>
2025-07-24 15:52:42 +0530

    24 July 2025

---
+++ src/ex_27_Enum/Locators.java
@@ -0,0 +1,16 @@
+package ex_27_Enum;
+
+public enum Locators {
+    page_input_email("#login-username"),
+    page_input_password("#login-password"),
+    page_button("#btn");
+
+    private String locators;
+    Locators (String locators){
+        this.locators = locators;
+    }
+    String getLocators(){
+        return this.locators;
+    }
+
+}
