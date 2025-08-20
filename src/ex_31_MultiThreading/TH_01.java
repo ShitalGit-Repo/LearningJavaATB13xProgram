040408c3b3a11f75283b1679eac1136c908273cb
IML-WKS-Admin <awacharshital94@gmail.com>
2025-08-20 11:22:03 -0500

    20 Aug 2025

---
+++ src/ex_31_MultiThreading/TH_01.java
@@ -0,0 +1,14 @@
+package ex_31_MultiThreading;
+
+public class TH_01 {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t = Thread.currentThread();
+        System.out.println(t);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+ "_"+t.getName());
+            System.out.println(i+ "_"+t.getPriority());
+            Thread.sleep(5000);
+        }
+    }
+}
